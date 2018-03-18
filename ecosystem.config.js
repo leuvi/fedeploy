5b0cf1399344d989d0586085e0ba40e041a80682
applebear <528046@qq.com>
2018-03-18 21:55:09 +0800

    add deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    {
+      name      : 'fruit',
+      script    : './server/index.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'root',
+      host : '47.104.182.121',
+      ref  : 'origin/master',
+      repo : 'https://github.com/leuvi/pm2.git',
+      path : '/opt/app/fruit',
+      'post-deploy' : 'yarn install && yarn build && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
